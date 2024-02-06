65cc594f761f3f7d12fc9c382575066282a4b806
Miller Mosquera <millerpalacios16@gmail.com>
2024-02-05 21:48:28 -0500

    data sending arrangement

--- src/main/java/com/example/springsecurity/controller/models/RegisterRequest.java
+++ src/main/java/com/example/springsecurity/controller/models/RegisterRequest.java
@@ -1,1 +1,1 @@
-package com.example.springsecurity.controller.models;import lombok.*;@Data@Builder@NoArgsConstructor@AllArgsConstructorpublic class RegisterRequest {    private String firstName;    private String lastName;    private String email;    private String password;}
+package com.example.springsecurity.controller.models;import lombok.*;@Data@Builder@NoArgsConstructor@AllArgsConstructorpublic class RegisterRequest {    private String first_name;    private String last_name;    private String email;    private String password;}
